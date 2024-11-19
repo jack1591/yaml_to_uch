5523485b18b2fba2603767b56474f6c66f4dff12
Artem <jack.tag@mail.ru>
2024-11-19 11:50:16 +0300

    count of string for comments

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <map>
 using namespace std;
 string yaml_path;
+int cnt;
 
 void outValues(const YAML::Node& values){
     for (auto c:values){
+        cnt++;
         string name = c.first.as<string>();
         string val = c.second.as<string>();
         cout << name << ": " << val << ";\n";
@@ -15,7 +17,8 @@ void outValues(const YAML::Node& values){
 void outDictionaries(const YAML::Node& values){
     for (auto dict:values){
         string name = dict.first.as<std::string>();
-        cout << name << ": \n{\n";
+        cnt++;
+        cout << name << " "<< cnt << " : \n{\n";
         const YAML::Node& dictionary = dict.second;
         outValues(dictionary);
         cout << "};\n";
@@ -25,16 +28,30 @@ void outDictionaries(const YAML::Node& values){
 void parseFile(const YAML::Node& node){
     for (const auto& item : node) {
         std::string key = item.first.as<std::string>();
+        cnt++;
         const YAML::Node& value = item.second;
         if (key=="constants"){
+            cout<<key<<" "<<cnt<<endl;
             outValues(value);
         }
         else if (key=="dictionaries"){
             outDictionaries(value);
         }
         else if (key=="calculations"){
-            cout<<key<<endl;
+            cout<<key<<" " <<cnt<<endl;
+            for (const auto& calc : value){
+                    std::string expression = calc.as<std::string>();
+                    std::cout << "Expression: " << expression << "\n";
+                    cnt++;
+            }
+            /*
+            for (auto c:value){
+                string out = c.as<string>();
+                cout<<out<<endl;
+            }
+            */
         }
+
     }   
 }
 
